26bab9d06fdb7d10d4382ee0f80029688ccc793f
Saurabh05Chauhan <92812918+Saurabh05Chauhan@users.noreply.github.com>
2024-01-30 20:53:33 +0530

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MyCafeBiller
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            StyleBundle(bundles);
+            ScriptBundle(bundles);
+        }
+
+        public static void StyleBundle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css")
+                     .Include("~/Content/bootstrap.css"));
+        }
+
+        public static void ScriptBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js")
+                     .Include("~/Scripts/jquery-{version}.js")
+                     .Include("~/Scripts/bootstrap.js"));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCafeBiller.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            List<string> food = new List<string>();
+            food.Add("Pizza");
+            ViewBag.foodItem=food;
+            return View();
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
